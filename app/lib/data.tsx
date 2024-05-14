28f8a8e6906953743ee6ae3bf30b1e030cabf750
corey <corey.cushnie@gmail.com>
2024-05-14 19:34:24 -0400

    add data

---
+++ app/lib/data.tsx
@@ -0,0 +1,58 @@
+export const socials = [
+  { name: "twitter", link: "/" },
+  { name: "youtube", link: "/" },
+  { name: "discord", link: "/" },
+  { name: "instagram", link: "/" },
+];
+
+export const footer = [
+  {
+    name: "Company",
+    tabs: [
+      "Our Story",
+      "How It Works",
+      "Pricing",
+      "Careers",
+      "Newsroom",
+      "Referral Program",
+      "Testimonials",
+    ],
+  },
+  {
+    name: "Support",
+    tabs: ["Community", "Contact Us", "Customers", "Feedback", "Join Discord"],
+  },
+  {
+    name: "Resources",
+    tabs: ["Blog", "Skills Explorer", "Learning Hub", "FAQs"],
+  },
+];
+
+Services: [
+  "Custom Software Development",
+  "Mobile App Development",
+  "Web Development",
+  "UI/UX Design",
+  "Quality Assurance & Testing",
+  "DevOps & Cloud Services",
+];
+
+AboutUs: [
+  "Our Story",
+  "Our Team",
+  "Careers",
+  "Testimonials",
+  "Partnerships",
+  "Contact Us",
+];
+
+Resources: [
+  "Blog",
+  "Case Studies",
+  "Whitepapers & Ebooks",
+  "Webinars & Events",
+  "FAQs",
+  "Support Center",
+];
+
+Connect: ["Subscribe to Newsletter"];
