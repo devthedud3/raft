79f17a3e697491063c5cfd60b113c193084d3dd3
corey <corey.cushnie@gmail.com>
2024-05-14 19:34:59 -0400

    init icons

---
+++ public/Icons.tsx
@@ -0,0 +1,13 @@
+import { FaXTwitter } from "react-icons/fa6";
+import {
+  PiInstagramLogoDuotone,
+  PiYoutubeLogoDuotone,
+  PiDiscordLogoFill,
+} from "react-icons/pi";
+
+export const IC: { [key: string]: any } = {
+  twitter: <FaXTwitter size={20} />,
+  instagram: <PiInstagramLogoDuotone size={20} />,
+  youtube: <PiYoutubeLogoDuotone size={20} />,
+  discord: <PiDiscordLogoFill size={20} />,
+};
