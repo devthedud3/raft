08c85eb0e29b8048ba9566928f5fe6cd7448f2ea
corey <corey.cushnie@gmail.com>
2024-05-15 14:51:49 -0400

    update logo

--- app/ui/components/Logo.tsx
+++ app/ui/components/Logo.tsx
@@ -3,5 +3,5 @@ import React from "react";
 type Props = {};
 
 export const Logo = (props: Props) => {
-  return <p className="text-md font-black text-white">RAFT</p>;
+  return <p className="text-lg font-black text-[--primary]">RAFT</p>;
 };
