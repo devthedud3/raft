f914cc0bf04af70d170f4e9cc07529de85dd50bd
corey <corey.cushnie@gmail.com>
2024-05-15 14:51:44 -0400

    fix header

--- app/ui/components/Header.tsx
+++ app/ui/components/Header.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 import { Button } from "./Button";
 import { Logo } from "./Logo";
+import Link from "next/link";
 
 type HeaderProps = {};
 
 export const Header = (props: HeaderProps) => {
   return (
-    <header className="absolute w-full flex justify-between py-10 px-24">
-      <Logo />
-      <Button name="Hire Engineers Now" />
+    <header className=" flex justify-center ">
+      <div className="py-5 px-24 flex items-center w-full justify-between">
+        <Logo />
+        <div className="inline-flex space-x-6 text-[--primary] items-center">
+          <div className="flex tex-sm group flex-col">
+            <Link href="/">Login</Link>
+            <div
+              className={`transition-w ease-in duration-100 border-b w-0 group-hover:border-black group-hover:w-full`}
+            />
+          </div>
+          <Button name="Schedule a demo" />
+        </div>
+      </div>
     </header>
   );
 };
